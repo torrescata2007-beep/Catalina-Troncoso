00cbbdc4241fa22548e06fb78b4b81e2826c760f
Catalina <torrescata2007@gmail.com>
2026-08-20 19:58:22 -0500

    feat(optimizacion): agrega optimizacion para no repetir el saludo

--- taller/app.js
+++ taller/app.js
@@ -7,3 +7,7 @@ function resaltarMenuActivo() {
   console.log("Resaltando el enlace activo del menu");
 }
 document.addEventListener("DOMContentLoaded", resaltarMenuActivo);
+let yaSaludo = false;
+function saludarUnaVez() {
+  if (!yaSaludo) { saludar(); yaSaludo = true; }
+}
